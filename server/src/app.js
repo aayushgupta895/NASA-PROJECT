8849779dbfe644015e62bd0e32031ea05de462cc
aayushgupta <guptaaayush895@gmail.com>
2022-07-12 19:10:07 +0530

    adding default routing by adding asterix

--- server/src/app.js
+++ server/src/app.js
@@ -17,8 +17,8 @@ app.use(cors({
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "public", "build")));
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '..','public','index.html'))
+app.get('/*', (req, res) =>{
+    res.sendFile(path.join(__dirname, '..','public','build','index.html'))
 })
 app.use(planetsRouter);
 app.use(launchesRouter);
