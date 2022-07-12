301b04101c4c772e33165c670e2c3be38535afcb
aayushgupta <guptaaayush895@gmail.com>
2022-07-12 18:19:58 +0530

    production build changes

--- server/src/app.js
+++ server/src/app.js
@@ -17,6 +17,9 @@ app.use(cors({
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "public", "build")));
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '..','public','index.html'))
+})
 app.use(planetsRouter);
 app.use(launchesRouter);
 
