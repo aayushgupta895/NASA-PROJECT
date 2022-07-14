48a512f012a5e06f9d8a3f1b3002da71e1d5259a
aayushgupta <guptaaayush895@gmail.com>
2022-07-15 00:43:32 +0530

    adding httpSubmitLaunch functionality

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -9,12 +9,25 @@ async function httpGetPlanets() {
 async function httpGetLaunches() {
    const response = await fetch(`${api_url}/launches`) 
     const fetchedLaunches =  await response.json()
-    return fetchedLaunches.sort((a, b) => a.flightNumber - b.flightNumber)
+    return fetchedLaunches.sort((a, b) => {
+      return  a.flightNumber - b.flightNumber}
+        )
 }
-
-async function httpSubmitLaunch(launch) {
-    // TODO: Once API is ready.
     // Submit given launch data to launch system.
+async function httpSubmitLaunch(launch) {
+    // console.log('here');
+    try{
+    return await fetch(`${api_url}/launches`,{
+        method : "post",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(launch)
+    })
+    }
+    catch(err){
+       return {ok : false ,}//json format
+    }
 }
 
 async function httpAbortLaunch(id) {
