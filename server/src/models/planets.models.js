d9c5e46061325ac0f5a016a6247de13479a813f6
aayushgupta895 <aayush19cs003@satiengg.in>
2022-08-16 13:10:59 +0530

    minor changes

--- server/src/models/planets.models.js
+++ server/src/models/planets.models.js
@@ -38,9 +38,9 @@ function loadPlanetsData() {
             .on('end', async () => {
                  const countPlanetsFound = (await getAllPlanets()).length
                 console.log(`${countPlanetsFound} habitable planet found`);
-                
+                resolve(); 
             });
-            resolve();
+            
     });
 }
 
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,6 +1,7 @@
 const { getAllPlanets } = require('../../models/planets.models')
 
 async function httpGetAllPlanets(req, res) {
+    
     console.log(await getAllPlanets())
     return res.status(200).json(await getAllPlanets());
 }
