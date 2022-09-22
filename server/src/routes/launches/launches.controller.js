3da0572df84b1b6dd9a69f1273b738279e4f3dce
aayushgupta895 <aayush19cs003@satiengg.in>
2022-09-23 00:40:02 +0530

    pagination service setup

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,10 +5,11 @@ const {
     existsLaunchWithId,
     abortLaunchById
           } = require('../../models/launches.models');
-
+const { getPagination } = require('../../services/query')
 async function httpGetAllLaunches(req, res) {
+    const {skip, limit}  = getPagination(req.query)
 
-    return res.status(200).json(await getAllLaunches());
+    return res.status(200).json(await getAllLaunches(skip, limit));
 };
 
 async function httpAddNewLaunch(req, res){
